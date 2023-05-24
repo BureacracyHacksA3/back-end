zepar-web/back-end-BH

modified todo list to send a predefined json and added postrequest to modify a user document status

src/main/java/com/bureaucracyhacks/refactorip/config/SecurityConfig.java
@@ -28,14 +28,14 @@ public class SecurityConfig {
                         try {
                             authorize
                                 .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
-                                    .requestMatchers(HttpMethod.POST, "/api/user-service/**").authenticated()
+                                    .requestMatchers("/api/user-service/**").permitAll()
                                     .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
-                                        .requestMatchers("/api/doc/**").authenticated()
-                                        .requestMatchers("/api/image/**").authenticated()
-                                        .requestMatchers("/api/institutions/**").authenticated()
-                                        .requestMatchers("/api/tasks/**").authenticated()
-                                        .requestMatchers("/api/directions").authenticated()
-                                            .anyRequest().authenticated()
+                                        .requestMatchers("/api/doc/**").permitAll()
+                                        .requestMatchers("/api/image/**").permitAll()
+                                        .requestMatchers("/api/institutions/**").permitAll()
+                                        .requestMatchers("/api/tasks/**").permitAll()
+                                        .requestMatchers("/api/directions").permitAll()
+                                            .anyRequest().permitAll()
                                     .and()
                                     .sessionManagement()
                                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)

BureacracyHacksA3/back-end

src/main/java/com/bureaucracyhacks/refactorip/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.bureaucracyhacks.refactorip.config;
 
-
 import com.bureaucracyhacks.refactorip.filters.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -33,8 +32,9 @@ public class SecurityConfig {
                                     .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
                                         .requestMatchers("/api/doc/**").authenticated()
                                         .requestMatchers("/api/image/**").authenticated()
-                                        .requestMatchers(HttpMethod.GET,"/api/institutions/**").authenticated()
-                                        .requestMatchers(HttpMethod.GET,"/api/tasks/**").authenticated()
+                                        .requestMatchers("/api/institutions/**").authenticated()
+                                        .requestMatchers("/api/tasks/**").authenticated()
+                                        .requestMatchers("/api/directions").authenticated()
                                             .anyRequest().authenticated()
                                     .and()
                                     .sessionManagement()